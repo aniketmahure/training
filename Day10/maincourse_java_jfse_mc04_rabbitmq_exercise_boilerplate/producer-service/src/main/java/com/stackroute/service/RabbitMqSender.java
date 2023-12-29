98b2f4bd79fcf954a834289f8f8b951d0f6becb7
Aniket Mahure <56632536+aniketmahure@users.noreply.github.com>
2023-12-29 13:03:04 +0530

    Add files via upload
    day 10

---
+++ Day10/maincourse_java_jfse_mc04_rabbitmq_exercise_boilerplate/producer-service/src/main/java/com/stackroute/service/RabbitMqSender.java
@@ -0,0 +1,42 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Employee;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMqSender {
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMqSender(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    /*
+     * to exchange and routingkey property values
+     */
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    public void send(Employee employee) {
+        /*
+        Convert the employee details and send to RabbitMQ
+         */
+        rabbitTemplate.convertAndSend(exchange,routingKey,employee);
+    }
+}
