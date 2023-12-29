8f525c54a692df722f4773b763930a8d731613f2
Aniket Mahure <56632536+aniketmahure@users.noreply.github.com>
2023-12-29 13:04:13 +0530

    Add files via upload
    day 11

---
+++ Day11/stack_fse_bridge_mc_microservice_log_aggregator_boilerplate/customer-service/src/main/java/com/stackroute/aggregator/customerservice/domain/Customer.java
@@ -0,0 +1,32 @@
+package com.stackroute.aggregator.customerservice.domain;
+
+import lombok.*;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String gender;
+    @Column
+    private String contactNumber;
+
+    public Customer(Integer id, String name, String email, String gender, String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.contactNumber = contactNumber;
+    }
+}
